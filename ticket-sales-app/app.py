6ff8d40d98fb61938ef6c67e432822684e5f37b8
juliafabri <juliafabrib@gmail.com>
2020-02-26 17:35:52 -0300

    ..

--- ticket-sales-app/app.py
+++ ticket-sales-app/app.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request,  url_for, redirect
+from concerts import Concerts
+
 app = Flask(__name__)
+
+foofighters = Concerts(band_name='foofighters')
+pearljam = Concerts(band_name='pearljam')
+
+bands = [foofighters, pearljam]
 
 @app.route('/index')
 def home():
     return render_template('index.html')
 
-@app.route('/register')
-def register_concert():
-    return render_template('concert_register.html')
+@app.route('/new', methods=['GET', 'POST'])
+def register():
+    if request.method == "POST":
+        bands.append(Concerts(request.form['band_name']))
+        return redirect(url_for('register'))
+    # if the method is GET, just return index.html
+    return render_template('concert_register.html', bands=bands)
+
+# @app.route('/data')
+# def list_concerts():
+#     band = request.args.get('band_name')
+#     return f"You put {band}"
+
+@app.route('/bands')
+def list_bands():
+    return render_template('list_concerts', bands=bands)
---
+++ ticket-sales-app/concerts.py
@@ -0,0 +1,6 @@
+class Concerts():
+
+    count = 1
+
+    def __init__(self, band_name):
+        self.band_name = band_name
