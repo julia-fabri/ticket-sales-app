b62dae2d6d03effa049c79173a3293c35f7fb5ea
juliafabri <juliafabrib@gmail.com>
2020-02-28 18:26:33 -0300

    insert form data into table

--- ticket-sales-app/app.py
+++ ticket-sales-app/app.py
@@ -1,20 +1,29 @@
 from flask import Flask, render_template, request,  url_for, redirect
-from concerts import Concerts
+import sqlite3
 
 app = Flask(__name__)
-
-foofighters = Concerts(band_name='foofighters')
-pearljam = Concerts(band_name='pearljam')
-
-concerts = [foofighters, pearljam]
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	return render_template('index.html')
 
-@app.route('/new', methods=['GET', 'POST'])
+@app.route('/new', methods=['GET'])
 def new():
 	return render_template('concert_register.html')
+
+@app.route('/create', methods=['POST'])
+def create():
+	if request.method=='POST':
+		conn = sqlite3.connect("tickets.db")
+		c = conn.cursor()
+		name = request.form['band_name']
+		print(name)
+		c.execute("INSERT INTO bands (name) VALUES (?)",(name,))
+		conn.commit()
+		print("inserted")
+		return "ok"
+	else:
+		return "não"
 
 @app.route('/list', methods=['GET', 'POST'])
 def list():
--- ticket-sales-app/config.py
+++ ticket-sales-app/config.py
@@ -7,29 +7,27 @@ c = conn.cursor()
 # Create table
 c.execute('''CREATE TABLE IF NOT EXISTS bands
              (
-             ID INT NOT NULL,
+             ID INTEGER PRIMARY KEY AUTOINCREMENT,
              NAME VARCHAR (100),
-             CATEGORY VARCHAR (100),
-             CONSTRAINT PK_BANDS PRIMARY KEY (ID)
+             CATEGORY VARCHAR (100)
              )''')
 
 
 c.execute('''CREATE TABLE IF NOT EXISTS concerts
              (
-             ID INT NOT NULL,
+             ID INTEGER PRIMARY KEY AUTOINCREMENT,
              BANDS_ID INT NOT NULL,
              LOCAL VARCHAR (250),
              DATE DATETIME,
              TICKETS_AVAILABLE,
-             CONSTRAINT PK_CONCERTS PRIMARY KEY (ID)
              CONSTRAINT FK_BANDS_ID FOREIGN KEY (BANDS_ID) REFERENCES BANDS(ID)
              )''')
 
 
-#
+
 # c.execute('''INSERT INTO bands VALUES
 #              ('2020-01-01', 'a', 'b', 0, 1)''')
-#
+
 c.execute("SELECT * FROM bands")
 rows = c.fetchall()
 print(rows)
