6f0ff8068a21ea05c9ed272d8e94e050b645668e
juliafabri <juliafabrib@gmail.com>
2020-02-28 17:46:35 -0300

    creates bands and concerts tables

--- ticket-sales-app/config.py
+++ ticket-sales-app/config.py
@@ -5,29 +5,31 @@ conn = sqlite3.connect("tickets.db")
 c = conn.cursor()
 
 # Create table
-c.execute('''CREATE TABLE stocks
-             (date text, trans text, symbol text, qty real, price real)''')
+c.execute('''CREATE TABLE IF NOT EXISTS bands
+             (
+             ID INT NOT NULL,
+             NAME VARCHAR (100),
+             CATEGORY VARCHAR (100),
+             CONSTRAINT PK_BANDS PRIMARY KEY (ID)
+             )''')
 
 
-c.execute('''INSERT INTO stocks VALUES
-             ('2020-01-01', 'a', 'b', 0, 1)''')
+c.execute('''CREATE TABLE IF NOT EXISTS concerts
+             (
+             ID INT NOT NULL,
+             BANDS_ID INT NOT NULL,
+             LOCAL VARCHAR (250),
+             DATE DATETIME,
+             TICKETS_AVAILABLE,
+             CONSTRAINT PK_CONCERTS PRIMARY KEY (ID)
+             CONSTRAINT FK_BANDS_ID FOREIGN KEY (BANDS_ID) REFERENCES BANDS(ID)
+             )''')
 
-c.execute("SELECT * FROM stocks WHERE trans = 'a'")
+
+#
+# c.execute('''INSERT INTO bands VALUES
+#              ('2020-01-01', 'a', 'b', 0, 1)''')
+#
+c.execute("SELECT * FROM bands")
 rows = c.fetchall()
 print(rows)
-
-
-# CREATE TABLE BANDS(
-# ID INT NOT NULL IDENTITY(1,1),
-# NAME VARCHAR (100),
-# CATEGORY VARCHAR (100),
-# CONSTRAINT PK_BANDS PRIMARY KEY (ID)
-# );
-# CREATE TABLE CONCERTS (
-# ID INT NOT NULL IDENTITY(1,1),
-# BANDS_ID INT NOT NULL,
-# LOCAL VARCHAR (250),
-# DATE DATETIME,
-# TICKETS_AVAILABLE,
-# CONSTRAINT PK_CONCERTS PRIMARY KEY (ID)
-# CONSTRAINT FK_BANDS_ID FOREIGN KEY (BANDS_ID) RE
