7dd87b7341c1663f2e2d737b0437c6f770aa06b8
juliafabri <juliafabrib@gmail.com>
2020-03-03 20:03:31 -0300

    .....

--- ticket-sales-app/app.py
+++ ticket-sales-app/app.py
@@ -8,17 +8,18 @@ def index():
 	conn = sqlite3.connect("tickets.db")
 	c = conn.cursor()
 
-	c.execute("SELECT bands.name, tickets.tickets_available FROM bands JOIN tickets WHERE tickets.bands_id = tickets.id")
+	c.execute("SELECT bands.name, concerts.local, concerts.tickets_available FROM bands JOIN concerts ON bands.id = concerts.bands_id")
 	data = c.fetchall()
+	print("MINHA DATA", data)
 
 	return render_template('index.html', data=data)
 
 @app.route('/new', methods=['GET'])
 def new():
-	return render_template('concert_register.html')
+	return render_template('band_register.html')
 
-@app.route('/create', methods=['POST'])
-def create():
+@app.route('/create_band', methods=['POST'])
+def create_band():
 	if request.method=='POST':
 		conn = sqlite3.connect("tickets.db")
 		c = conn.cursor()
@@ -27,52 +28,64 @@ def create():
 		band_name = request.form['band_name']
 		category = request.form['category']
 		#concerts db
-		local = request.form['local']
-		date = request.form['date']
-		time = request.form['time']
+		# local = request.form['local']
+		# date = request.form['date']
+		# time = request.form['time']
 
 		c.execute("INSERT INTO bands (name, category) VALUES (?, ?)",(band_name, category))
-		c.execute("SELECT id FROM bands")
-		band_id = c.fetchall()
+		#
+		# c.execute("SELECT id FROM bands")
+		# band_id = c.fetchall()
 
-		for band_id in band_id:
-			c.execute("INSERT INTO concerts (bands_id, local, date, time) VALUES (?, ?, ?, ?)",(band_id[0], local, date, time,))
+		# for band_id in band_id:
+		# 	c.execute("INSERT INTO concerts (bands_id, local, date, time) VALUES (?, ?, ?, ?)",(band_id[0], local, date, time,))
 		conn.commit()
 
-		c.execute("SELECT bands.id, bands.name, concerts.local, concerts.date, bands.category FROM concerts JOIN bands ON concerts.bands_id = bands.id")
-		name = c.fetchall()
-		print("meu name", name)
-		conn.close()
-
-		return render_template('list_concerts.html', name=name)
+		return redirect(url_for('list_bands'))
 	else:
 		return "erro"
 
-@app.route('/list', methods=['GET', 'POST'])
-def list():
+@app.route('/list_bands', methods=['GET', 'POST'])
+def list_bands():
 	conn = sqlite3.connect("tickets.db")
 	c = conn.cursor()
-
 	c.execute("SELECT * FROM bands")
-	name = c.fetchall()
+	data = c.fetchall()
 
-	return render_template('list_concerts.html', name=name)
+	return render_template('list_bands.html', data=data)
 
-@app.route('/delete', methods=['POST', 'DELETE'])
-def delete():
+@app.route('/delete_band', methods=['POST', 'DELETE'])
+def delete_band():
 	if request.method == "POST":
 		if request.form['action'] == 'Delete':
 			band_id = request.form['id']
 			conn = sqlite3.connect("tickets.db")
-			c = conn.cursor()
-			print(band_id)
-			c.execute("DELETE FROM bands WHERE id = (?)", (band_id,))
-			conn.commit()
-			c.execute("SELECT * FROM bands")
-			name = c.fetchall()
 
-			return render_template('list_concerts.html', name=name)
+			return render_template('list_bands.html', name=name)
 	return "erro"
 
+
+@app.route('/edit_band/<id>')
+def edit_band(id):
+	conn = sqlite3.connect("tickets.db")
+	c = conn.cursor()
+
+	c.execute("SELECT * FROM bands WHERE id = (?)", (id,))
+	band = c.fetchall()
+
+	return render_template('concert_register_update.html', band=band)
+
+@app.route('/update_band', methods=['POST', 'DELETE'])
+def update_band():
+	id = request.form['id']
+	band_name = request.form['band_name']
+	category = request.form['category']
+
+	conn = sqlite3.connect("tickets.db")
+	c = conn.cursor()
+	c.execute("UPDATE bands SET name = (?), category = (?) WHERE id = (?)", (band_name, category, id))
+	conn.commit()
+
+	return redirect(url_for('list_bands'))
 
 app.run(debug=True)
--- ticket-sales-app/config.py
+++ ticket-sales-app/config.py
@@ -18,22 +18,11 @@ c.execute('''CREATE TABLE IF NOT EXISTS concerts
              ID INTEGER PRIMARY KEY AUTOINCREMENT,
              BANDS_ID INT NOT NULL,
              LOCAL VARCHAR (250),
+             TICKETS_AVAILABLE INT,
              TIME time,
              DATE DATE,
              CONSTRAINT FK_BANDS_ID FOREIGN KEY (BANDS_ID) REFERENCES BANDS(ID)
              )''')
-
-
-c.execute('''CREATE TABLE IF NOT EXISTS tickets
-             (
-             ID INTEGER PRIMARY KEY AUTOINCREMENT,
-             BANDS_ID INT NOT NULL,
-             CONCERTS_ID INT NOT NULL,
-             TICKETS_AVAILABLE,
-             CONSTRAINT FK_BANDS_ID FOREIGN KEY (BANDS_ID) REFERENCES BANDS(ID)
-             CONSTRAINT FK_CONCERTS_ID FOREIGN KEY (CONCERTS_ID) REFERENCES CONCERTS(ID)
-             )''')
-
 
 # c.execute('''INSERT INTO bands VALUES
 #              ('2020-01-01', 'a', 'b', 0, 1)''')
